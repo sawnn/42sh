c27cb14395c51ecbad67459f1162cb0d6e3cadb9
Daniel <daniel@localhost.localdomain>
2018-05-25 22:29:39 +0200

    guillemet

--- lib/my_str_to_word_array.c
+++ lib/my_str_to_word_array.c
@@ -1,66 +1,68 @@
 /*
-** EPITECH PROJECT, 2018
-** 42sh
+** EPITECH PROJECT, 2017
+** my_str_to_word_array.c
 ** File description:
-** no_param
+** a
 */
 
 #include "../include/my_sh.h"
 
-int	num_word(char *str)
+int	calcule(char const *str)
 {
-	int	i = 0;
-	int	word = 0;
+	int i = 1;
+	int k = 0;
+	int tmp = 0;
 
-	while (str[i] != '\0') {
-		while (str[i] != '\0' && str[i++] == ' ');
-		++word;
-		while (str[i] != '\0' && str[i++] > ' ');
-	}
-	return (word);
-}
-
-int	n_cha(char *str, int i)
-{
-	int	j = i;
-
-	if (str[i] > 32 && str[i] != 127) {
-		while (str[i] > 32 && str[i] != 127)
+	while (str[k] != '\0') {
+		if (str[k] == '"')
+			tmp++;
+		if ((str[k] < 33 || str[k] > 126) && !(tmp % 2)) {
 			i++;
-	}
-	return (i - j);
-}
-
-int	skip_escape(char *str, int i)
-{
-	if (str[i] == ' ') {
-		while (str[i] == ' ')
-			i = i + 1;
+		}
+		k++;
 	}
 	return (i);
 }
 
+char	**cut(char *str, char *pick, char **bac, int i)
+{
+	int k = -1;
+	int j = 0;
+	int a = 0;
+	int tmp = 0;
+
+	while (pick && bac && ++k < my_strlen(str)) {
+		i = k;
+		
+		while ((str[k] >= 33 && str[k] <= 126) || (tmp % 2)) {
+			if (str[k] == '"') {
+				tmp++;
+				k++;
+			} else
+				pick[a++] = str[k++];
+		}
+		if (str[k] < 33 && i != k) {
+			pick[a++] = '\0';
+			bac[j++] = strdup(pick);
+		}
+		a = 0;
+	}
+	bac[j] = NULL;
+	return (bac);
+}
+
 char	**my_str_to_word_array(char *str)
 {
-	int	i = 0;
-	int	x = 0;
-	int	y = 0;
-	int	word = num_word(str);
-	char	**tab = malloc(sizeof(char *) * (word + 2));
+	char *pick = NULL;
+	char **bac = NULL;
+	int i = 0;
 
-	if (!tab)
+	if (str == NULL)
 		return (NULL);
-	while (y != word) {
-		i = skip_escape(str, i);
-		if ((tab[y] = malloc(sizeof(char) * n_cha(str, i) + 1)) == NULL)
-			return (NULL);
-		while (str[i] > 32 && str[i] != '\0')
-			tab[y][x++] = str[i++];
-		tab[y][x] = '\0';
-		i = i + 1;
-		y = y + 1;
-		x = 0;
-	}
-	tab[word] = NULL;
-	return (tab);
+	pick = malloc(sizeof(char) * (my_strlen(str) + 1));
+	bac = malloc(sizeof(char*) * (calcule(str) + (sizeof(char*) * 2)));
+	if (bac == NULL || pick == NULL)
+		return (NULL);
+	bac = cut(str, pick, bac, i);
+	return (bac);
 }
