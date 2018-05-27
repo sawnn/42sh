37d4077eb9a9b311dcd79707bd1e403d1bd56217
valentin <valentin.decroix@epitech.eu>
2018-05-27 14:16:33 +0200

    dollars

--- src/builtins/dollars.c
+++ src/builtins/dollars.c
@@ -16,11 +16,42 @@ char	*after_char(char *str, char c)
 	return (&str[i + 1]);
 }
 
+int	count_bracket(char *str)
+{
+	int i = 0;
+	int nb = 0;
+
+	while (str[i]) {
+		if (str[i] == '{' || str[i] == '}')
+			nb += 1;
+		i += 1;
+	}
+	return (nb);
+}
+
+char	*check(char *str)
+{
+	int count = count_bracket(str);
+	char *tmp = malloc(sizeof(char) * my_strlen(str) - count + 1);
+	int i = 0;
+	int j = 0;
+
+	while (str[i]) {
+		if (str[i] != '{' && str[i] != '}') {
+			tmp[j] = str[i];
+			j += 1;
+		}
+		i += 1;
+	}
+	return (tmp);
+}
+
 char	*dollars(char *str, t_set *list, node **head)
 {
 	t_set *tmp = list;
 	node *env = (*head);
 
+	str = check(str);
 	while (tmp) {
 		if (strcmp(tmp->op, str) == 0)
 			return (tmp->value);
